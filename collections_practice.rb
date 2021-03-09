f38dbd8eea13b86477c380e46c3f00f1dfa43dc7
meaghan jennings <meaghanljennings@gmail.com>
2021-03-09 20:52:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,37 +2,51 @@ def sort_array_asc(array)
   array.sort
 end
 
+
 def sort_array_desc(array)
-  array.sort! {|x,y| y <=> x}
-end 
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
 
 def sort_array_char_count(array)
-  res = array.sort {|a, b| a.length <=> b.length}
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
 end
+
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
-  return array 
+  return array
 end
+
 
 def reverse_array(array)
   array.reverse
 end
 
+
 def kesha_maker(array)
   new_array = []
   array.each do |string|
     string[2] = "$"
-    new_array << string
+    new_array << string 
   end
   new_array
 end
 
-
 def find_a(array)
-  array.select do |string|
-    string.start_with?("a")
-  end 
+  array.select do |word|
+    word.start_with?("a")
+  end
 end
 
 def sum_array(array)
@@ -41,11 +55,11 @@ end
 
 def add_s(array)
   new_array = []
-  array.each_with_index do |word, index|
-    if index != 1
-      word = word + "s"
+  array.each_with_index do |w, i|
+    if i != 1 
+      w = w + "s"
     end
-    new_array << word
-  end 
+    new_array << w 
+    end 
   new_array
-end
+end 
